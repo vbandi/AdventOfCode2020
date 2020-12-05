913e440cf26869d02ac259df27bf8bbfa05c5faa
Velvárt András <andras.velvart@aerinx.com>
2020-12-05 15:37:50 +0100

    Day5

---
+++ AdventOfCode2020/Day5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+            var seatIDs = input.Select(GetSeatID).ToList();
+            var max = seatIDs.Max();
+            Console.WriteLine($"Part 1: {max}");
+
+            var emptySeats = Enumerable.Range(0, max).Except(seatIDs);
+            var mySeat = emptySeats.Where(id => seatIDs.Contains(id + 1) && seatIDs.Contains(id - 1));
+            
+            Console.WriteLine($"Part2: {mySeat.First()}");
+        }
+
+        private static int GetSeatID(string code)
+        {
+            string binary = code.Replace('F', '0').Replace('B', '1').Replace('R', '1').Replace('L', '0');
+            return Convert.ToInt32(binary, 2);
+        }
+    }
+}
