d0f7429ac25cf58bc5b280ac3b9392afea1cfb9c
Velvárt András <andras.velvart@aerinx.com>
2020-12-07 22:40:32 +0100

    Day6

---
+++ AdventOfCode2020/Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("Input.txt");
+            input = input.Replace("\r\n", " ").Replace("  ", "\n");
+            
+            var records = input.Split('\n').Select(s => s.Trim().Replace(" ", "")).ToArray();
+
+            var result = records.Sum(s => s.Distinct().Count());
+            
+            Console.WriteLine($"Part 1: {result}");
+
+            records = input.Split('\n').Select(s => s.Trim()).ToArray();
+            result = records.Sum(s => s.Distinct().Count(c => s.Split(' ').All(a => a.Contains(c))));
+            Console.WriteLine($"Part 2: {result}");
+
+        }
+    }
+}
