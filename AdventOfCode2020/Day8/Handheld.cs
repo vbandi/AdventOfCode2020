7e992d94a5ef427e3b1190f2dc53db0d2a6c5916
Velvárt András <andras.velvart@aerinx.com>
2020-12-08 18:57:18 +0100

    Day 8

---
+++ AdventOfCode2020/Day8/Handheld.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    public class Handheld
+    {
+        public List<Instruction> Memory { get; } = new ();
+        public int Accumulator { get; private set; } = 0;
+        public int PC { get; private set; } = 0;
+
+        public void Compile(string input)
+        {
+            foreach (var line in input.Split(Environment.NewLine.ToCharArray()))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+                
+                // Console.WriteLine($"Processing '{line}'");
+                if (!Enum.TryParse(line[..3], out Operation op))
+                    throw new InvalidOperationException(line[..3]);
+
+                if (!int.TryParse(line[4..], out int argument))
+                    throw new ArgumentException(line);
+                
+                Memory.Add(new Instruction(op, argument));
+            }
+        }
+
+        
+        /// <summary>
+        /// Executes the instructions in the memory
+        /// </summary>
+        /// <returns>true if the program finished - false if infinite loop was detected</returns>
+        public bool Run()
+        {
+            List<int> ExecutedInstructions = new();
+            
+            PC = 0;
+            Accumulator = 0;
+
+            while (PC < Memory.Count)
+            {
+                if (ExecutedInstructions.Contains(PC))
+                    return false;
+
+                ExecutedInstructions.Add(PC);
+
+                if (Execute(Memory[PC]))
+                    PC++;
+            }
+            
+            Console.WriteLine("Program reached end.");
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the specified instruction
+        /// </summary>
+        /// <returns>true, if the PC needs to be incremented</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        private bool Execute(Instruction instruction)
+        {
+            switch (instruction.Operation)
+            {
+                case Operation.acc:
+                    Accumulator += instruction.Argument;
+                    return true;
+                case Operation.jmp:
+                    PC += instruction.Argument;
+                    return false;
+                case Operation.nop:
+                    return true;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+    
+    public record Instruction(Operation Operation, int Argument);
+
+    public enum Operation
+    {
+        // ReSharper disable InconsistentNaming
+        acc, jmp, nop
+        // ReSharper restore InconsistentNaming
+    }
+}
---
+++ AdventOfCode2020/Day8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Day8;
+
+var input = File.ReadAllText("Input.txt");
+var handheld = new Handheld();
+handheld.Compile(input);
+handheld.Run();
+Console.WriteLine($"Part 1 - accumulator at infinite loop: {handheld.Accumulator}");
+
+for (int i = 0; i < handheld.Memory.Count; i++)
+{
+    SwapJmpAndNop(i);
+
+    if (handheld.Run())
+    {
+        Console.WriteLine($"Part 2 - accumulator at end {handheld.Accumulator}");
+        return;
+    }
+    
+    SwapJmpAndNop(i);
+}
+
+void SwapJmpAndNop(int address)
+{
+    var operation = handheld.Memory[address].Operation;
+
+    handheld.Memory[address] = operation switch
+    {
+        Operation.jmp => handheld.Memory[address] with {Operation = Operation.nop},
+        Operation.nop => handheld.Memory[address] with {Operation = Operation.jmp},
+        _ => handheld.Memory[address]
+    };
+}
